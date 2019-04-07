70a3292fe9677397a7641c8333c10ad01ec694d6
pranavhari2 <42246450+pranavhari2@users.noreply.github.com>
2019-04-07 18:53:11 -0400

    Update Library.cpp

--- Library.cpp
+++ Library.cpp
@@ -1,7 +1,11 @@
+
 #include "Library.h"
 #include "Client.h"
-#include "Librarian.h"
 #include "Book.h"
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
 
 Library::Library()
 {
@@ -13,6 +17,108 @@ Library::~Library()
     //dtor
 }
 
-void AddBook(Book);
-void DeleteBook(Book);
-void ModifyBook(Book);
+// Uploads edited book list to txt file and replaces the original with the new one.
+void Library::uploadBooks(Book book)
+{
+    ofstream myfile;
+    myfile.open("Library Database.txt" , ios::out | ios::app);
+    if (myfile.is_open())
+    {
+        myfile << "1" << "," << book.getTitle() << "," << book.getAuthor() << "," << book.getDescription() << "," << book.getNumofPages() << endl;
+        myfile.close();
+    }
+    else
+    {
+        cout << "Could not open file." << endl;
+    }
+}
+
+void Library::AddBook(/*Book book*/)
+{
+    string title;
+    string auth;
+    string desc;
+    int pages;
+
+  cout << "Title: ";
+  getline(cin, title);
+  cout << "Author: ";
+  getline(cin, auth);
+  cout << "Description (keep it short): ";
+  getline(cin, desc);
+  cout << "Num of pages: ";
+  cin >> pages;
+  Book book(title, auth, desc, pages);
+
+    uploadBooks(book);
+
+  ifstream file;
+  string line;
+  file.open("Library Database.txt");
+  if (file.is_open())
+  {
+    while ( getline (file,line,',') )
+    {
+        cout << line << endl;
+    }
+    file.close();
+  }
+
+  else cout << "Unable to open file";
+
+  }
+
+void Library::DeleteBook(Book books[])
+{
+
+}
+void Library::ModifyBook(Book books[])
+{
+
+}
+
+void Library::ClientMenu()
+{
+  /*
+  string name;
+  string password;
+  bool login;
+
+  cout << "                                 Enter New Login Information:" << endl << endl;
+
+  cout << "Name: ";
+  cin >> name;
+  cout << "\n";
+  cout << "Password: ";
+  cin >> password;
+  cout << "\n";
+  Client client(name, password);
+  cout << "Username: " << client.getName() << endl;
+  cout << "Password: " << client.getPassword() << endl;
+
+  while(true)
+  {
+      cout << "                                 Enter Login Information:" << endl << endl;
+
+  cout << "Name: ";
+  cin >> name;
+  cout << "\n";
+  cout << "Password: ";
+  cin >> password;
+  cout << "\n";
+
+  login = client.Login(name, password);
+  if (login == 1)
+  {
+      break;
+  }
+  }
+  */
+  AddBook();
+
+
+  return;
+
+}
+
+
